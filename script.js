36a8f38fc14208eacebc671b5e48c90addcea884
brendon977 <65783017+brendon977@users.noreply.github.com>
2020-07-20 19:40:37 -0300

    Contador

---
+++ script.js
@@ -0,0 +1,36 @@
+function contar(){
+    let ini = window.document.getElementById('txti')
+    let fim = window.document.getElementById('txtf')
+    let passo = window.document.getElementById('txtp')
+    let res = window.document.getElementById('res')
+
+
+    if(ini.value.length == 0 || fim.value.length== 0 || passo.value.length== 0){ // para que a caixa de texto no site não fique em branco este é o comando com o ((nome da var).value.lenght==0)
+        res.innerHTML = "Impossível contar !"
+        window.alert("ERROR, faltam dados!!")
+        
+    } else{
+        res.innerHTML = 'Contando: <br> '
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if (p <= 0){
+            window.alert("Passo inválido! Considerando passo 1")
+            p = 1
+        }
+        if (i < f) {
+            //Contagem crescente
+            for( let c = i; c <= f; c += p){
+                res.innerHTML += `${c} \u{1F449}` // codigo para colocar emoji
+            }
+
+        } else {
+            // Contagem regressiva
+            for( let c = i; c >= f; c -= p){
+                res.innerHTML += `${c} \u{1F449}`
+            }
+        } res.innerHTML += `\u{1F3C1}`
+
+    }    
+
+} 
